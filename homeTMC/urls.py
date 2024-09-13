2a8262d07941057a69d56c5f29f990bee8b37ac4
viniciusdantasdc <viniemmovimento@gmail.com>
2024-09-13 01:55:34 -0300

    removed templante contato and agenda

--- homeTMC/urls.py
+++ homeTMC/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
-from homeTMC.views import home, agenda, contato
+from homeTMC.views import home
 
 urlpatterns = [
     path('', home), # HOME
-    path('agenda/', agenda), # /agenda/
-    path('contato/', contato), # /contato/
+
 ]
 
 # video 23 - movendo urls para views
--- homeTMC/views.py
+++ homeTMC/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'homeTMC/home.html')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-def agenda(request):
-    return HttpResponse('AGENDA')
+    return render(request, 'homeTMC/home.html', context={
+        'name': 'Apollo Dantas'
+    })
